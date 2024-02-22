780660e4d3e9cfc2f5d1fd834e39f9ad34e9604e
bogotolec <bogotolec2000@gmail.com>
2024-02-23 02:33:25 +0300

    Added tiles falling

--- assets/scripts/gameplay/PlayableArea.ts
+++ assets/scripts/gameplay/PlayableArea.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, EventMouse, instantiate, math, Node, Prefab, SpriteFrame, sys, UITransform } from 'cc';
+import { _decorator, Component, EventMouse, instantiate, math, Node, Prefab, SpriteFrame, sys, tween, UITransform, Vec3, warn } from 'cc';
 import { Tile } from './Tile';
 import { Queue } from '../utils/Queue';
 const { ccclass, property } = _decorator;
@@ -17,6 +17,7 @@ export class PlayableArea extends Component {
     private _colors = 2
     private _minGroupSize = 2
     private _nodeSize = 1100 / 12
+    private _tileMoveSpeed = 5
 
     private _y0 = -535
 
@@ -66,6 +67,10 @@ export class PlayableArea extends Component {
     private getGroup(this, x: number, y: number) {
         let result: [number, number][] = []
 
+        if (!this._field[y][x]) {
+            return result
+        }
+
         let desiredColor = this._field[y][x].getColor()
 
         let visited: boolean[][] = []
@@ -82,7 +87,8 @@ export class PlayableArea extends Component {
             this._groupDirections.forEach(([diffX, diffY]) => {
                 let newX = curX + diffX, newY = curY + diffY
 
-                if (this._field[newY] && this._field[newY][newX] && this._field[newY][newX].getColor() == desiredColor &&
+                if (this._field[newY] && this._field[newY][newX] && !this._field[newY][newX].isFalling &&
+                    this._field[newY][newX].getColor() == desiredColor &&
                     !(visited[newY] && visited[newY][newX])) {
                     queue.enqueue([newX, newY])
 
@@ -97,14 +103,47 @@ export class PlayableArea extends Component {
         return result
     }
 
+    private moveTile(this, tile: Tile, toX: number, toY: number) {
+
+        let fromX = tile.x, fromY = tile.y
+
+        let [toPosX, toPosY] = this.getPositionFromCoords(toX, toY)
+        let fromPosX = tile.node.position.x, fromPosY = tile.node.position.y
+
+        let distance = Math.sqrt((toPosX - fromPosX) ** 2 + (toPosY - fromPosY) ** 2)
+
+        this._field[fromY][fromX] = null
+
+        this._field[toY][toX] = tile
+        tile.x = toX
+        tile.y = toY
+        tile.isFalling = true
+
+        tween(tile.node)
+            .to(distance / (this._tileMoveSpeed * this._nodeSize), { position: new Vec3(toPosX, toPosY) }, {
+                onComplete: () => {
+                    tile.isFalling = false
+                }
+            })
+            .start()
+    }
+
     private onHover(this, event: EventMouse) {
+
         let tile = event.target.getComponent(Tile)
+
+        if (tile.isFalling) {
+            return
+        }
+
         let x = tile.x, y = tile.y
 
         let group = this.getGroup(x, y)
         if (group.length >= this._minGroupSize) {
             group.forEach(([x, y]) => {
-                this._field[y][x].highlight()
+                if (this._field[y][x]) {
+                    this._field[y][x].highlight()
+                }
             })
         }
     }
@@ -112,7 +151,42 @@ export class PlayableArea extends Component {
     private onHoverEnd(this, event: EventMouse) {
         for (let y = 0; y < this._height; ++y) {
             for (let x = 0; x < this._width; ++x) {
-                this._field[y][x].unhighlight()
+                if (this._field[y][x]) {
+                    this._field[y][x].unhighlight()
+                }
+            }
+        }
+    }
+
+    private onClick(this, event: EventMouse) {
+        let tile = event.target.getComponent(Tile)
+        
+        if (tile.isFalling) {
+            return
+        }
+
+        let x = tile.x, y = tile.y
+
+        let group = this.getGroup(x, y)
+
+        if (group.length >= this._minGroupSize) {
+            group.forEach(([x, y]) => {
+                if (this._field[y][x]) {
+                    this._field[y][x].node.destroy()
+                    this._field[y][x] = null
+                }
+            })
+
+            for (let x = 0; x < this._width; ++x) {
+                let toY = 0
+                for (let y = 0; y < this._height; ++y) {
+                    if (this._field[y][x]) {
+                        if (toY != y) {
+                            this.moveTile(this._field[y][x], x, toY)
+                        } 
+                        toY += 1
+                    }
+                }
             }
         }
     }
@@ -137,6 +211,7 @@ export class PlayableArea extends Component {
 
                 tile.on(Node.EventType.MOUSE_ENTER, this.onHover, this, tileComponent)
                 tile.on(Node.EventType.MOUSE_LEAVE, this.onHoverEnd, this, tileComponent)
+                tile.on(Node.EventType.MOUSE_DOWN, this.onClick, this, tileComponent)
             }
         }
     }
